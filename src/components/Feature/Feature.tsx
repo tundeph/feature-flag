55b369902acd026503d83298af1122fcf4005e67
tundeph <54335828+tundeph@users.noreply.github.com>
2022-02-23 10:35:02 +0000

    Update Feature.tsx

--- src/components/Feature/Feature.tsx
+++ src/components/Feature/Feature.tsx
@@ -1,113 +1,137 @@
-import React, { useState } from "react"
-import { FeatureChild } from "./FeatureChild"
+import React, { Dispatch, SetStateAction, useCallback } from "react"
+
+//components
+import FeatureChild from "../FeatureChild/FeatureChild"
+
+//types
+import { FeatureType } from "./Feature.types"
+
+//styles
 import "./Feature.scss"
 
 type SettingsProps = {
-  settings: {
-    feature: string
-    status: boolean
-    quantity?: number
-    maxQuantity?: number
-    children: {
-      feature: string
-      status: boolean
-      quantity?: number
-      maxQuantity?: number
-    }[]
-  }[]
+  defaultValue: FeatureType[]
+  getValue: Dispatch<SetStateAction<FeatureType[]>>
 }
 
-const Feature = ({ settings }: SettingsProps) => {
-  const [values, setValues] = useState(settings)
-  console.log(values)
-
-  const handleStatus = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const isChecked = event.target.checked
-    const featureId = event.target.value
-
-    const val = values.map((value) => {
-      if (value.children.length > 0) {
-        value.children.map((childValue) => {
-          if (childValue.feature === featureId) {
-            childValue.status = isChecked
+const Feature = ({ defaultValue, getValue }: SettingsProps) => {
+  const handleParentStatus = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>, feature: string) => {
+      const isChecked = event.target.checked
+      if (defaultValue.length > 0) {
+        const val = defaultValue.map((value) => {
+          if (value.feature === feature) {
+            value.status = isChecked
+            if (!isChecked && value.children) {
+              value.children.map((childValue) => {
+                childValue.status = false
+                childValue.quantity = 0
+                return childValue
+              })
+              value.quantity = 0
+            }
           }
-          return childValue
+          return value
         })
+        getValue(val)
       }
+    },
+    [defaultValue, getValue]
+  )
 
-      if (value.feature === featureId) {
-        value.status = isChecked
-        if (!isChecked) {
+  const handleChildrenStatus = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>, feature: string) => {
+      const isChecked = event.target.checked
+      const val = defaultValue.map((value) => {
+        if (value.children) {
           value.children.map((childValue) => {
-            childValue.status = false
+            if (childValue.feature === feature) {
+              childValue.status = isChecked
+            }
             return childValue
           })
         }
-      }
-      return value
-    })
-    setValues(val)
-  }
+        return value
+      })
+      getValue(val)
+    },
+    [defaultValue, getValue]
+  )
 
-  const handleFrequency = (
+  const handleParentFrequency = (
     event: React.ChangeEvent<HTMLSelectElement>,
-    featureId?: string
+    feature: string
   ) => {
-    const val = values.map((value) => {
-      if (value.children.length > 0) {
-        value.children.map((childValue) => {
-          if (childValue.feature === featureId) {
-            childValue.quantity = Number(event.target.value)
-          }
-          return childValue
-        })
-      }
-
-      if (value.feature === featureId) {
+    const val = defaultValue.map((value) => {
+      if (value.feature === feature) {
         value.quantity = Number(event.target.value)
       }
       return value
     })
+    getValue(val)
+  }
 
-    setValues(val)
+  const handleChildrenFrequency = (
+    event: React.ChangeEvent<HTMLSelectElement>,
+    feature: string
+  ) => {
+    const freq = Number(event.target.value)
+    const value = defaultValue.map((v) => {
+      if (v.children) {
+        v.children.map((childValue) => {
+          if (childValue.feature === feature) {
+            childValue.quantity = freq
+          }
+          return childValue
+        })
+      }
+      return v
+    })
+    getValue(value)
   }
 
   return (
-    <div className="feature-parent ">
+    <div className="feature-parent" data-testid="feature">
       <div>
         <div className="feature">
-          {settings &&
-            values.map((setting, index) => (
+          {defaultValue &&
+            defaultValue.map((setting, index) => (
               <React.Fragment key={index}>
                 <div
-                  className={
-                    setting.children.length > 0 ? "top-split" : "top-no-split"
-                  }
+                  className={setting.children ? "top-split" : "top-no-split"}
                 >
                   <FeatureChild
                     feature={setting.feature}
                     status={setting.status}
                     maxQuantity={setting.maxQuantity}
-                    handleStatus={handleStatus}
-                    handleFrequency={(e) => handleFrequency(e, setting.feature)}
+                    handleStatus={(e) => handleParentStatus(e, setting.feature)}
+                    handleFrequency={(e) =>
+                      handleParentFrequency(e, setting.feature)
+                    }
                   />
-                  {setting.children.length > 0 && (
+                  {setting.children && (
                     <div className="top-arrow">
-                      <i className="arrow arrow-down"></i>
+                      {setting.status ? (
+                        <i className="arrow arrow-up"></i>
+                      ) : (
+                        <i className="arrow arrow-down"></i>
+                      )}
                     </div>
                   )}
                 </div>
                 {setting.status &&
-                  setting.children.length > 0 &&
+                  setting.children &&
                   setting.children.map((child, index) => (
                     <div className="bottom" key={index}>
                       <FeatureChild
                         feature={child.feature}
                         status={child.status}
                         maxQuantity={child.maxQuantity}
-                        handleStatus={handleStatus}
+                        handleStatus={(e) =>
+                          handleChildrenStatus(e, child.feature)
+                        }
                         handleFrequency={(e) =>
-                          handleFrequency(e, child.feature)
+                          handleChildrenFrequency(e, child.feature)
                         }
                       />
                     </div>
@@ -120,4 +144,4 @@ const Feature = ({ settings }: SettingsProps) => {
   )
 }
 
-export default Feature
+export default React.memo(Feature)
